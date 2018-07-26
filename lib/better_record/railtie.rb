bdb9150ee1912ad9eedc78417937db4c9a56f5e8
Sampson Crowley <sampsonsprojects@gmail.com>
2018-07-26 15:22:36 -0600

    no need to yell when rake_task runs

--- lib/better_record/railtie.rb
+++ lib/better_record/railtie.rb
@@ -1,9 +1,7 @@
 module BetterRecord
   class Railtie < ::Rails::Railtie
     rake_tasks do
-      puts 'BetterRecord::Railtie RAKE TASKS'
       Dir[File.expand_path('tasks/**/*.rake', __dir__)].each do |f|
-        puts f
         load f
       end
     end
--- lib/better_record/version.rb
+++ lib/better_record/version.rb
@@ -1,3 +1,3 @@
 module BetterRecord
-  VERSION = '0.5.4'
+  VERSION = '0.5.5'
 end
