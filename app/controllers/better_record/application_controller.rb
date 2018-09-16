135b75401088acb786c7364344ccf305190fae46
Sampson Crowley <sampsonsprojects@gmail.com>
2018-09-16 14:02:52 -0600

    add

--- app/controllers/better_record/application_controller.rb
+++ app/controllers/better_record/application_controller.rb
@@ -1,6 +1,16 @@
 module BetterRecord
   class ApplicationController < ActionController::Base
+    # == Modules ============================================================
+
+    # == Class Methods ======================================================
+
+    # == Pre/Post Flight Checks =============================================
     protect_from_forgery with: :exception
     layout BetterRecord.layout_template.presence || 'better_record/application'
+
+    # == Actions ============================================================
+
+    # == Cleanup ============================================================
+
   end
 end
--- app/controllers/better_record/table_sizes_controller.rb
+++ app/controllers/better_record/table_sizes_controller.rb
@@ -2,17 +2,23 @@ require_dependency "better_record/application_controller"
 
 module BetterRecord
   class TableSizesController < ApplicationController
+    # == Modules ============================================================
+
+    # == Class Methods ======================================================
+
+    # == Pre/Post Flight Checks =============================================
     before_action :set_table_size, only: [:show]
 
-    # GET /table_sizes
+    # == Actions ============================================================
     def index
       TableSize.reload_data if Boolean.parse(params[:reload])
       @table_sizes = TableSize.all
     end
 
-    # GET /table_sizes/1
     def show
     end
+
+    # == Cleanup ============================================================
 
     private
       # Use callbacks to share common setup or constraints between actions.
